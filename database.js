8f49510663e1e7b52cda9576d4418dff0bff0edd
vinaysharma <vs864606@gmail.com>
2024-10-29 20:37:18 +0530

    add .env files

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost', 
   user: process.env.DB_USER || 'root',       
   password: process.env.DB_PASS || 'Vinay@6378',  
-  database: process.env.DB_NAME || 'BloodBank'
+  database: process.env.DB_NAME || 'BloodBank',
+  port: process.env.DB_PORT || 3306
 });
 connection.connect((err) => {
   if (err) {
